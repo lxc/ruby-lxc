892e1637b1cb5051684ea42d5b89cee62dd0e32d
Andre Nathan <andre@digirati.com.br>
2013-12-06 09:37:29 -0200

    Accept string arguments

--- ext/lxc.c
+++ ext/lxc.c
@@ -1160,6 +1160,9 @@ lxc_run_command(VALUE self, VALUE rb_command)
     lxc_attach_command_t cmd;
     VALUE rb_program;
 
+    if (TYPE(rb_command) == T_STRING)
+        rb_command = rb_str_split(rb_command, " ");
+
     rb_program = rb_ary_entry(rb_command, 0);
     cmd.program = StringValuePtr(rb_program);
     cmd.argv = ruby_to_c_string_array(rb_command);
