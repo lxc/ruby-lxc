b077728c7d42f87bce17145c8b10f3ebe82f5dc4
Ranjib Dey <ranjib@pagerduty.com>
2014-01-03 18:22:19 -0800

    drop shared library inside lxc folder

--- ext/lxc/extconf.rb
+++ ext/lxc/extconf.rb
@@ -4,4 +4,4 @@ abort 'missing liblxc' unless find_library('lxc', 'lxc_container_new')
 abort 'missing lxc/lxccontainer.h' unless have_header('lxc/lxccontainer.h')
 
 $CFLAGS += " -Wall #{ENV['CFLAGS']}"
-create_makefile('lxc')
+create_makefile('lxc/lxc')
